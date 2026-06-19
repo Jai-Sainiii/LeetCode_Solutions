cd72c93e2aef169dcad9d89d3352757789bc0132
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-19 16:54:32 +0530

    Time: 0 ms (100%), Space: 55.9 MB (86.19%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let count = 1
+    let num = nums[0]
+
+    for (let i = 1; i < nums.length; i++) {
+        if(count === 0){
+            num = nums[i]
+            count++
+        }else{
+            if(num === nums[i]){
+                count++
+            }else{
+                count--
+            }
+        }
+    }
+
+    return num
+};
