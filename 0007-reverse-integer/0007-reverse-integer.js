ca4e7d9ff73c4fed2e4a0aaff541d9302ca99532
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-21 21:17:50 +0530

    Time: 63 ms (10.62%), Space: 55.3 MB (83.76%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let revNum = 0
+
+    if(x > -(2 ** 31) || x < (2 ** 31)){
+        let check = Math.sign(x)
+        if(check < 0) x = -x
+        else x = x
+
+        while(x > 0){
+            lastDigit = x % 10
+            revNum = revNum * 10 + lastDigit
+            x = Math.floor( x / 10 )
+        }
+
+        if(check < 0) revNum = -revNum
+
+        if(revNum >= -(2 ** 31) && revNum <= (2 ** 31)){
+            return revNum
+        }else{
+            return 0
+        }
+    }else{
+        return 0
+    }
+};
