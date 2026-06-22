ae0f24a5fd715b5ce1017f96a9d34243d400f5b8
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-22 16:36:08 +0530

    Time: 2 ms (20.29%), Space: 59.2 MB (5.85%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,13 +4,12 @@
  */
 var removeDuplicates = function(nums) {
     if(nums.length === 1) return 1
- 
-    let count = 0
-    let check = -Infinity;
-    let left = 0;
 
-    for (let right = 0; right < nums.length; right++) {
-        if(nums[right] > check){
+    let count = 1
+    let left = 1;
+
+    for (let right = 1; right < nums.length; right++) {
+        if(nums[right] > nums[left - 1]){
             check = nums[right];
             [nums[left], nums[right]] = [nums[right], nums[left]];
             count++
