adfeabb332d4002ddb7622adc28ef329029d1df5
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-22 19:41:13 +0530

    Time: 8 ms (43.9%), Space: 60.7 MB (35.21%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -3,6 +3,7 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
+    if(nums.length === 1) return [nums[0]]
     let min = Math.floor(nums.length / 3)
     let numbers = []
     let map = new Map()
