6b8072a37f6d5d9da64647e6547216e45e3d1c1c
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-22 16:40:55 +0530

    Time: 0 ms (100%), Space: 56.9 MB (82.51%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,14 +3,12 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    if(nums.length === 1) return 1
-
     let left = 0;
 
     for (let right = 1; right < nums.length; right++) {
         if(nums[right] !== nums[left]){
             left++;
-            [nums[left], nums[right]] = [nums[right], nums[left]];
+            nums[left] = nums[right]
         };
     };
 
