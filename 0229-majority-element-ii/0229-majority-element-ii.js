81b736edcc0652af9e8d44eacb98b954d6f7cafb
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-22 20:25:08 +0530

    Time: 2 ms (95.12%), Space: 56.8 MB (81.55%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -3,16 +3,42 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    if(nums.length === 1) return [nums[0]]
     let min = Math.floor(nums.length / 3)
     let numbers = []
-    let map = new Map()
+
+    let el1 = Number.MAX_SAFE_INTEGER, el2 = Number.MIN_SAFE_INTEGER;
+
+    let c1 = 0, c2 = 0;
 
     for(const num of nums){
-        map.set(num, (map.get(num) || 0) + 1)
-        if(map.get(num) === min+1){
-            numbers.push(num)
+        if(num === el1){
+            c1++
+        }else if(num === el2){
+            c2++
+        }else if(c1 === 0){
+            el1 = num
+            c1 = 1
+        }else if(c2 === 0){
+            el2 = num
+            c2 = 1
+        }else{
+            c1--
+            c2--
         }
+    }
+
+    c1 = 0, c2 = 0;
+
+    for(const num of nums){
+        if(num === el1) c1++
+        if(num === el2) c2++
+    }
+
+    if(c1 > min){
+        numbers.push(el1)
+    }
+    if(c2 > min){
+        numbers.push(el2)
     }
 
     return numbers
