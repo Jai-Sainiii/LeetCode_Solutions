26245d32675093f89225b133d3b1bf6cd482becc
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-23 14:50:53 +0530

    Time: 12 ms (89.88%), Space: 57.8 MB (54.2%) - LeetHub

--- 0018-4sum/0018-4sum.js
+++ 0018-4sum/0018-4sum.js
@@ -4,18 +4,18 @@
  * @return {number[][]}
  */
 var fourSum = function(nums, target) {
+    let n = nums.length
+    nums.sort((a,b) => a - b)
     let array = []
 
-    nums.sort((a,b) => a - b)
-
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < n; i++) {
         if(i > 0 && nums[i] === nums[i-1]) continue
-        for (let j = i+1; j < nums.length; j++) {
+        for (let j = i+1; j < n-1; j++) {
 
             if(j > i+1 && nums[j] === nums[j-1]) continue
 
             let left = j + 1
-            let right = nums.length - 1
+            let right = n - 1
 
             while(left < right){
                 let sum = nums[i] + nums[j] + nums[left] + nums[right]
