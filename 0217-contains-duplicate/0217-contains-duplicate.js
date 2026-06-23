4f1522c7c1b9d3c5f5535db80a095df5ba828f90
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-23 23:44:55 +0530

    Time: 14 ms (84.89%), Space: 73 MB (87.47%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -5,11 +5,8 @@
 var containsDuplicate = function(nums) {
     let set = new Set()
     for(const num of nums){
-        if(set.has(num)){
-            return true
-        }else{
-            set.add(num)
-        }
+        if(set.has(num)) return true
+        else set.add(num)
     }
     return false
 };
