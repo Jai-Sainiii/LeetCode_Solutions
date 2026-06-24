031a736fcee1e42fb579b93d700a4669bc651acb
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-24 16:31:08 +0530

    Time: 1 ms (90.91%), Space: 52.3 MB (99.84%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,17 +3,18 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let max = -Infinity
+    let n = nums.length
 
+    let max = -Infinity
     let leftProd = 1
     let rightProd = 1
 
-    for(let i = 0; i < nums.length; i++){
+    for(let i = 0; i < n; i++){
         if(leftProd === 0) leftProd = 1
         if(rightProd === 0) rightProd = 1
 
         leftProd *= nums[i]
-        rightProd *= nums[nums.length-i-1]
+        rightProd *= nums[n-i-1]
 
         max = Math.max(max, leftProd, rightProd)
     }
