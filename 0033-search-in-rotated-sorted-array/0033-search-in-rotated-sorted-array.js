ea6709ad4bce3cbb3084e8344220c62a0952b56f
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-24 20:12:31 +0530

    Time: 1 ms (11.34%), Space: 53.6 MB (73.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,8 +4,8 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    low = 0
-    high = nums.length-1
+    let low = 0
+    let high = nums.length-1
     while(low <= high){
         let mid = Math.floor((low+high)/1)
         if(nums[mid] === target){
