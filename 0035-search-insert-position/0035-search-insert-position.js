4bc857ba83f94ab51850c6c2cba5395b9cdf857d
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-24 17:46:59 +0530

    Time: 0 ms (100%), Space: 53.8 MB (58.62%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let ans = nums.length
+    let low = 0
+    let high = nums.length-1
+
+    while(low <= high){
+        let mid = Math.floor((low+high)/2)
+        if(nums[mid] >= target){
+            ans = mid
+            high = mid - 1
+        }else{
+            low = mid + 1
+        }
+    } 
+
+    return ans
+};
