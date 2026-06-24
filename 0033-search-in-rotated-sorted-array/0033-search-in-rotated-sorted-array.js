ec4d6c4c03e11361408ac29bd7ecf16d255da028
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-24 20:11:23 +0530

    Time: 1 ms (11.34%), Space: 54.3 MB (21.39%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,32 +4,28 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    if(nums.length-1 === 0 && nums[0] === target) return 0
-    else if(nums.length-1 === 0 && nums[0] !== target) return -1
+    if(nums.length === 0 && nums[0] === target) return 0
+    else if(nums.length === 0 && nums[0] !== target) return -1
 
-    let mid = -1
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] < nums[i+1]){
-            continue
-        }else{
-            mid = i + 1
-            break
-        }
-    }
-
-    let low = 0
-    let high = nums.length - 1
-    if(mid === -1) mid = Math.floor((low + high) / 2)
-
+    low = 0
+    high = nums.length-1
     while(low <= high){
+        let mid = Math.floor((low+high)/1)
         if(nums[mid] === target){
             return mid
-        }else if(target >= nums[mid] && target <= nums[high]){
-            low = mid + 1
-            mid = Math.floor((low + high) / 2)
+        }
+        if(nums[low] <= nums[mid]){
+            if(target < nums[mid] && nums[low] <= target){
+                high = mid - 1
+            }else{
+                low = mid + 1
+            }
         }else{
-            high = mid - 1
-            mid = Math.floor((low + high) / 2)
+            if(target > nums[mid] && nums[high] >= target){
+                low = mid + 1
+            }else{
+                high = mid - 1
+            }
         }
     }
 
