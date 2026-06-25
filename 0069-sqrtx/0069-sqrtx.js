ac7a1bf50c7161a7e262d8aeaed9ba4de8d5416f
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-25 21:13:56 +0530

    Time: 1 ms (74.1%), Space: 55.8 MB (61.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,5 +3,22 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    return Math.floor(Math.sqrt(x))
+    let low = 0
+    let high = x
+    let ans = 0
+
+    while(low <= high){
+        let mid = Math.floor((low+high)/2)
+
+        if(mid*mid === x){
+            return mid
+        }else if(mid*mid < x){
+            ans = mid
+            low = mid+1
+        }else{
+            high = mid-1
+        }
+    }
+
+    return ans
 };
