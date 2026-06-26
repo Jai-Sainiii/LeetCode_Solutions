be78f8835e70f28f2832526242056cd31dc6cce8
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-26 17:33:52 +0530

    Time: 15 ms (52.73%), Space: 64.4 MB (72.73%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.js
+++ 2716-minimize-string-length/2716-minimize-string-length.js
@@ -3,9 +3,5 @@
  * @return {number}
  */
 var minimizedStringLength = function(s) {
-    let set = new Set()
-    for(let i = 0; i < s.length; i++){
-        set.add(s[i])
-    }
-    return set.size
+    return new Set(s).size
 };
