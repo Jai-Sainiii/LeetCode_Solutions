f399e37aea1e459f3f7402888c87cb78c077704c
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-27 17:32:28 +0530

    Time: 20 ms (86.94%), Space: 67.8 MB (85.07%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.js
+++ 0575-distribute-candies/0575-distribute-candies.js
@@ -3,10 +3,6 @@
  * @return {number}
  */
 var distributeCandies = function(candyType) {
-    let set = new Set()
-    for(const candy of candyType){
-        set.add(candy)
-    }
-    let canEat = candyType.length/2
-    return Math.min(set.size, canEat)
+    let set = new Set(candyType)
+    return Math.min(set.size, candyType.length/2)
 };
