9c6062159bb6074d780967853a1773a956f17b6f
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-27 19:08:34 +0530

    Time: 6 ms (85.71%), Space: 59.7 MB (19.05%) - LeetHub

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} threshold
+ * @return {number}
+ */
+var smallestDivisor = function(nums, threshold) {
+    let low = 1
+    let high = Math.max(...nums)
+    while(low <= high){
+        let mid = Math.floor((low+high)/2)
+
+        let sum = 0
+        for(const num of nums){
+            sum += Math.ceil(num/mid)
+        }
+        if(sum <= threshold){
+            high = mid-1
+        }else{
+            low = mid+1
+        }
+    }
+    return low
+};
