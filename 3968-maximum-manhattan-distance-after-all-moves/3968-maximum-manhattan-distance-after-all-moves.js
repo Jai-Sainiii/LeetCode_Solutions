d6bc0cb4ed5c3a8d69e0ef0caf985e613d370f76
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-27 23:57:14 +0530

    Time: 23 ms (46.9%), Space: 60.2 MB (36.43%) - LeetHub

--- 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.js
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.js
@@ -6,30 +6,18 @@ var maxDistance = function(moves) {
     let leftRight = 0
     let upDown = 0
     let count = 0
-    for(let i = 0; i < moves.length; i++){
-        if(moves[i] === "U"){
+    for(const move of moves){
+        if(move === "U"){
             upDown++
-        }else if(moves[i] === "D"){
+        }else if(move === "D"){
             upDown--
-        }else if(moves[i] === "R"){
+        }else if(move === "R"){
             leftRight++
-        }else if(moves[i] === "L"){
+        }else if(move === "L"){
             leftRight--
-        }else if(moves[i] === "_"){
+        }else if(move === "_"){
             count++
         }
     }
-    // while(count > 0){
-    //     if(leftRight < 0){ leftRight--; count--; continue}
-    //     else if(upDown < 0){ upDown--; count--; continue }
-    //     else if(leftRight >= 0){ leftRight++; count--; continue}
-    //     else if(upDown >= 0){ upDown++; count--; continue}
-    //     else {upDown++; count--; continue}
-    // }
-
-    // if(leftRight < 0) leftRight = -leftRight
-    // if(upDown < 0) upDown = -upDown
-    // return leftRight + upDown
-
     return Math.abs(leftRight) + Math.abs(upDown) + count
 };
