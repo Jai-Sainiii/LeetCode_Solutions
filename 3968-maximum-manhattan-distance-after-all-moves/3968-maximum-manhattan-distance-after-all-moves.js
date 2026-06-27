a1160913a10b8cf9f553301a4ee4a1909100ce4d
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-27 23:56:10 +0530

    Time: 20 ms (69.77%), Space: 59.9 MB (60.85%) - LeetHub

--- 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.js
+++ 3968-maximum-manhattan-distance-after-all-moves/3968-maximum-manhattan-distance-after-all-moves.js
@@ -16,37 +16,20 @@ var maxDistance = function(moves) {
         }else if(moves[i] === "L"){
             leftRight--
         }else if(moves[i] === "_"){
-            // if(leftRight === 0 && upDown === 0){ 
-            //     upDown++
-            //     continue
-            // }
-            // else if(leftRight < 0 && upDown < 0){ 
-            //     leftRight--
-            //     continue
-            // }
-            // else if(leftRight >= 0 && upDown >= 0){ 
-            //     upDown++
-            //     continue
-            // }
-            // else if(leftRight < 0 && upDown >= 0){
-            //     if(-leftRight >= upDown) leftRight--
-            //     else upDown++
-            // }
-            // else if(leftRight >= 0 && upDown < 0){
-            //     if(leftRight >= -upDown) upDown--
-            //     else leftRight++
-            // }
             count++
         }
     }
-    while(count > 0){
-        if(leftRight < 0){ leftRight--; count--; continue}
-        else if(upDown < 0){ upDown--; count--; continue }
-        else if(leftRight >= 0){ leftRight++; count--; continue}
-        else if(upDown >= 0){ upDown++; count--; continue}
-        else {upDown++; count--; continue}
-    }
-    if(leftRight < 0) leftRight = -leftRight
-    if(upDown < 0) upDown = -upDown
-    return leftRight + upDown
+    // while(count > 0){
+    //     if(leftRight < 0){ leftRight--; count--; continue}
+    //     else if(upDown < 0){ upDown--; count--; continue }
+    //     else if(leftRight >= 0){ leftRight++; count--; continue}
+    //     else if(upDown >= 0){ upDown++; count--; continue}
+    //     else {upDown++; count--; continue}
+    // }
+
+    // if(leftRight < 0) leftRight = -leftRight
+    // if(upDown < 0) upDown = -upDown
+    // return leftRight + upDown
+
+    return Math.abs(leftRight) + Math.abs(upDown) + count
 };
