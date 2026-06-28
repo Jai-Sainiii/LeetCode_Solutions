b66c28c0bebf9ab62d313d4afd4d7b3325071af7
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-28 12:05:03 +0530

    Time: 9 ms (44.44%), Space: 66.2 MB (33.33%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.js
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.js
@@ -5,16 +5,13 @@
 var maximumElementAfterDecrementingAndRearranging = function(arr) {
     arr.sort((a,b) => a-b)
     if(arr[0] !== 1) arr[0] = 1
-    let ans = 1
     for(let i = 1; i < arr.length; i++){
         if((arr[i] - arr[i-1]) <= 1) {
-            ans = Math.max(ans, arr[i])
             continue
         }
         else{
             arr[i] = arr[i-1] + 1
-            ans = Math.max(ans, arr[i])
         }
     }
-    return ans
+    return arr[arr.length-1]
 };
