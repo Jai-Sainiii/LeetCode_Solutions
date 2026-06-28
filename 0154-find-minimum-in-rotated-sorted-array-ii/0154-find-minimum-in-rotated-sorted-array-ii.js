1bf94e8d988ae54dc4e4289ede5d008803f3527f
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-28 16:06:51 +0530

    Time: 0 ms (100%), Space: 53.9 MB (55.53%) - LeetHub

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let low = 0
+    let high = nums.length-1
+    while(low < high){
+        let mid = Math.floor((low+high)/2)
+        // if(nums[mid] === nums[low]){
+        //     low++
+        //     continue
+        // }
+        if(nums[mid] === nums[high]){
+            high--
+            continue
+        }
+        if(nums[mid] > nums[high]){
+            low = mid+1
+        }else{
+            high = mid
+        }
+    }
+    return nums[low]
+};
