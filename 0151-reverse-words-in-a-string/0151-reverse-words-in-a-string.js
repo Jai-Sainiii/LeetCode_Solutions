80ad3ab1f70f7ca07c7cbd3661ccccc950ff4c13
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-30 23:48:22 +0530

    Time: 3 ms (39.98%), Space: 57.2 MB (21.38%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,13 +3,17 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    s = s.trim().split(" ").reverse()
-
     let result = ""
-    for(let i = 0; i < s.length; i++){
-        if(s[i] !== "" && s[i] !== " " && i !== s.length-1) result += s[i] + " "
-        else if(i === s.length-1) result += s[i]
-        else continue
+    let i = s.length-1
+    while(i >= 0){
+        while(i >= 0 && s[i] === " ") i--
+        if(i < 0) break
+        let end = i
+        while(i >= 0 && s[i] !== " ") i--
+        let word = s.substring(i+1, end+1)
+        if(result.length > 0) result += " "
+        result += word
     }
+    
     return result
 };
