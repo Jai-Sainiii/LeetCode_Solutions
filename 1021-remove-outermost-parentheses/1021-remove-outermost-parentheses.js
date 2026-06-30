9f00791fbe5e76206dd111f5a63a71e604e0e7dd
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-30 23:15:29 +0530

    Time: 4 ms (60.62%), Space: 57.7 MB (21.66%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function(s) {
+    let result = ""
+    let level = 0
+    for(const char of s){
+        if(char === "("){
+            if(level > 0) result += char
+            level++
+        }else if(char === ")"){
+            level--
+            if(level > 0) result += char
+        }
+    }
+    return result
+};
