319fe0b4e62d315bf50ad568ceaf6ee7fa4c4117
Jai-Sainiii <jaisaini6166@gmail.com>
2026-06-30 12:34:25 +0530

    Time: 1 ms (20.73%), Space: 67 MB (37.81%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.js
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.js
@@ -3,15 +3,30 @@
  * @return {number[]}
  */
 var findPeakGrid = function(mat) {
-    let max = -Infinity
-    let ans = [1,1]
-    for(let i = 0; i < mat.length; i++){
-        for(let j = 0; j < mat[0].length; j++){
-            if(mat[i][j] > max){
-                max = mat[i][j]
-                ans = [i,j]
+    let ans = [-1,-1]
+    function maxElement(matrix, col){
+        let max = -Infinity
+        let row = -1
+        for(let i = 0; i < matrix.length; i++){
+            if(matrix[i][col] > max){
+                max = matrix[i][col]
+                row = i
             }
         }
+        return row
+    }
+    let low = 0
+    let high = mat[0].length-1
+    while(low <= high){
+        let mid = Math.floor((low+high)/2)
+        let row = maxElement(mat, mid)
+
+        let leftVal = mid > 0 ? mat[row][mid-1] : -Infinity
+        let rightVal = mid < mat[0].length-1 ? mat[row][mid+1] : -Infinity
+
+        if(mat[row][mid] > leftVal && mat[row][mid] > rightVal) return [row, mid]
+        else if(leftVal > mat[row][mid]) high = mid-1
+        else low = mid+1
     }
     return ans
 };
