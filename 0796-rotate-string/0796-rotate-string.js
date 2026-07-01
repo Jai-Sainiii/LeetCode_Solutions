116fc65c5d1cd76d3fd0f52e707fde1bbb4b58db
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-01 12:46:03 +0530

    Time: 0 ms (100%), Space: 54.3 MB (22.43%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -5,13 +5,7 @@
  */
 var rotateString = function(s, goal) {
     if(s.length !== goal.length) return false
-    let arr = [...s]
-
-    for (let i = 0; i < s.length; i++) {
-        let char = arr[0]
-        arr.shift()
-        arr.push(char)
-        if(arr.join("") === goal) return true
-    }
-    return false
+    s = s+s
+    if(s.includes(goal)) return true
+    else return false
 };
