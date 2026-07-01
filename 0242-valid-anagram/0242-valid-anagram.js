5b83848d10db1e6f10418364dfb528d787f76711
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-01 19:02:56 +0530

    Time: 12 ms (71.37%), Space: 55.8 MB (73.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -10,11 +10,7 @@ var isAnagram = function(s, t) {
 
     for(let i = 0; i < s.length; i++){
         map.set(s[i], (map.get(s[i]) || 0) + 1)
-    }
-
-    for(let i = 0; i < t.length; i++){
-        if(!map.has(t[i])) return false
-        else map.set(t[i], map.get(t[i]) - 1)
+        map.set(t[i], (map.get(t[i]) || 0) - 1)
     }
 
     for(const [key, value] of map){
