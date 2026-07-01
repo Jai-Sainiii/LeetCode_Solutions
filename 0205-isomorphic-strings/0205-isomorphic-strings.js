be01993d5bffb13c9ed1f6909862dc67796b46e3
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-01 12:23:53 +0530

    Time: 4 ms (90.72%), Space: 54.6 MB (72.87%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,15 +4,12 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    let map = new Map()
+    let map1 = new Map()
     let map2 = new Map()
     for(let i = 0; i < s.length; i++){
-        if(map.has(s[i]) || map2.has(t[i])){
-            if(map.get(s[i]) === t[i]) continue
-            else if(map.get(t[i]) === s[i]) continue
-            else return false
-        }else{
-            map.set(s[i], t[i])
+        if(map1.has(s[i]) && map1.get(s[i]) !== t[i] || map2.has(t[i]) && map2.get(t[i]) !== s[i]) return false
+        else{
+            map1.set(s[i], t[i])
             map2.set(t[i], s[i])
         }
     }
