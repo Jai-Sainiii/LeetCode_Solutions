cdd0926fee4dd05b06ed6b5ccbbdee70b171c598
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-04 19:47:18 +0530

    Time: 36 ms (0.62%), Space: 53.9 MB (32.7%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let current = head
+    if(current.next === null) return current
+
+    let count = 0
+    while(current !== null){
+        count++
+        current = current.next
+    }
+    let mid = Math.floor(count/2)
+
+    count = 0
+    current = head
+    while(count !== mid){
+        count++
+        current = current.next
+    }
+    return current
+};
