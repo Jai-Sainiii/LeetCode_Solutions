23b7ccd5123a439468e6f2bc284498eda6371616
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-05 15:05:25 +0530

    Time: 1 ms (91.38%), Space: 56.4 MB (81.71%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -3,18 +3,14 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-   let arr = []
-    arr.push(nums[0])
-    for (let i = 1; i < nums.length; i++) {
-        arr[i] = arr[i-1] + nums[i] 
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-        if(i === 0 && arr[arr.length-1] - arr[0] === 0) return 0
-        sum = arr[arr.length-1] - arr[i + 1]
-        if(arr[i] === sum){
-            return i+1
+    let total = nums.reduce((acc, val) => acc+val, 0)
+    let leftSum = 0
+    for(let i = 0; i < nums.length; i++){
+        let rightSum = total-leftSum-nums[i]
+        if(leftSum===rightSum){
+            return i
         }
+        leftSum += nums[i]
     }
     return -1
 };
