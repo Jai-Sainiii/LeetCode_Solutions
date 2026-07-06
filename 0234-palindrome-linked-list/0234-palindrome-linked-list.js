9c9d7d721d97e21d367711a4db4f393aaf5d1259
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-06 12:29:03 +0530

    Time: 90 ms (7.4%), Space: 83 MB (30.17%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let stack = []
+    let current = head
+    while(current){
+        stack.push(current.val)
+        current = current.next
+    }
+    return stack.join("") === stack.reverse().join("")
+};
