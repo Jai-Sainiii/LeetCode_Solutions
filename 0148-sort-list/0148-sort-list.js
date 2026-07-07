d66d9590bb2033634bae03f9a530d5cd6a1297fc
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-07 14:18:33 +0530

    Time: 47 ms (10%), Space: 78.6 MB (76.05%) - LeetHub

--- 0148-sort-list/0148-sort-list.js
+++ 0148-sort-list/0148-sort-list.js
@@ -10,17 +10,48 @@
  * @return {ListNode}
  */
 var sortList = function(head) {
-    let arr = []
-    let current = head
-    while(current){
-        arr.push(current.val)
-        current = current.next
+    if(head === null || head.next === null) return head
+
+    function merge(list1, list2){
+        let dummyNode = new ListNode(-1)
+        let temp = dummyNode
+        while(list1 && list2){
+            if(list1.val <= list2.val){
+                temp.next = list1
+                list1 = list1.next
+            }else{
+                temp.next = list2
+                list2 = list2.next
+            }
+            temp = temp.next
+        }
+        if(list1){
+            temp.next = list1
+        }else{
+            temp.next = list2
+        }
+        return dummyNode.next
     }
-    arr.sort((a,b) => b-a)
-    current = head
-    while(current){
-        current.val = arr.pop()
-        current = current.next
+
+    function findMiddle(head){
+        if(head === null || head.next === null) return head
+
+        let slow = head
+        let fast = head.next
+        while(fast && fast.next){
+            slow = slow.next
+            fast = fast.next.next
+        }
+        return slow
     }
-    return head
+
+    let middle = findMiddle(head)
+
+    let right = middle.next
+    middle.next = null
+    let left = head
+    left = sortList(left)
+    right = sortList(right)
+
+    return merge(left, right)
 };
