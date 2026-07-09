4521e00f07a03159d22140924ee2d35350a821c5
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-10 00:35:07 +0530

    Time: 5 ms (63.67%), Space: 56.4 MB (73.7%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
@@ -5,7 +5,7 @@
  */
 var numSubarraysWithSum = function(nums, goal) {
     function countSubArray(nums, goal){
-        if(goal < 0) return 0
+        if(goal < 0) return 0;
         let left = 0;
         let sum = 0;
         let count = 0;
@@ -18,7 +18,7 @@ var numSubarraysWithSum = function(nums, goal) {
 
             count += (right - left + 1);
         }
-        return count
+        return count;
     }
-    return countSubArray(nums, goal) - countSubArray(nums, goal-1)
+    return countSubArray(nums, goal) - countSubArray(nums, goal-1);
 };
