d45de06a52c4f2bbe83b94f0bbbf5a53238479ec
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-09 14:26:56 +0530

    Time: 1 ms (97.89%), Space: 63 MB (14.24%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.js
@@ -4,14 +4,20 @@
  * @return {number}
  */
 var longestOnes = function(nums, k) {
+    let count = 0
     let max = 0
-    for(let i = 0; i < nums.length; i++){
-        let zeros = 0
-        for(let j = i; j < nums.length; j++){
-            if(nums[j] === 0) zeros++
-            if(zeros > k) break
-            max = Math.max(max, j-i+1)
+    let left = 0
+    let right = 0
+    while(right < nums.length){
+        if(nums[right] === 0){
+            count++
         }
+        while(count > k){
+            if(nums[left] === 0) count--
+            left++
+        }
+        max = Math.max(max, right-left+1)
+        right++
     }
     return max
 };
