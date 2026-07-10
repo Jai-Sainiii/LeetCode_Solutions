9943ea2dbe2500d9ee2d4f1b3b615edf5726ed61
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-10 23:56:23 +0530

    Time: 6 ms (57.11%), Space: 63 MB (16.94%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,39 +3,35 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    if(nums.length === 1){
-        nums[0] *= nums[0]
-        return nums
-    }
-    let arr = []
-    let left = -1
-    let right = -1
+    let arr = [];
+    let left = -1;
+    let right = -1;
     for(let i = 0; i < nums.length; i++){
-        if(nums[i] <= 0){ 
-            left = i
-            right = i+1
+        if(nums[i] <= 0 && nums.length !== 1){ 
+            left = i;
+            right = i+1;
         }
-        nums[i] *= nums[i]
+        nums[i] *= nums[i];
     }
 
-    if(left === -1) return nums
+    if(left === -1) return nums;
     
-    let count = 0
+    let count = 0;
     while(left >= 0 && right < nums.length){
         if(nums[left] <= nums[right]){
-            arr[count] = nums[left--]
+            arr[count] = nums[left--];
         }else{
-            arr[count] = nums[right++]
+            arr[count] = nums[right++];
         }
-        count++
+        count++;
     }
     while(left >= 0){
-        arr[count] = nums[left--]
-        count++
+        arr[count] = nums[left--];
+        count++;
     }
     while(right < nums.length){
-        arr[count] = nums[right++]
-        count++
+        arr[count] = nums[right++];
+        count++;
     }
-    return arr
+    return arr;
 };
