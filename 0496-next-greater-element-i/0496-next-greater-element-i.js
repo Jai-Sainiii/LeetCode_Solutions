0fa616d66ee397def026a10bad676951e40914cf
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-14 13:04:36 +0530

    Time: 2 ms (87.47%), Space: 57.4 MB (60.78%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,23 +4,23 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let map = new Map()
+    let map = new Map();
     for(let i = 0; i < nums1.length; i++){
-        map.set(nums1[i], i)
+        map.set(nums1[i], i);
     }
 
-    let stack = []
+    let monotonicStack = [];
     for(let i = nums2.length-1; i >= 0; i--){
-        let num = nums2[i]
-        while(stack.length && stack[stack.length-1] <= num){
-            stack.pop()
+        let num = nums2[i];
+        while(monotonicStack.length && monotonicStack[monotonicStack.length-1] <= num){
+            monotonicStack.pop();
         }
 
-        if(stack.length === 0) nums1[map.get(num)] = -1
-        else nums1[map.get(num)] = stack[stack.length-1]
+        if(monotonicStack.length === 0) nums1[map.get(num)] = -1;
+        else nums1[map.get(num)] = monotonicStack[monotonicStack.length-1];
 
-        stack.push(num)
+        monotonicStack.push(num);
     }
 
-    return nums1
+    return nums1;
 };
