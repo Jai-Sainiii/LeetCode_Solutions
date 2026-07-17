fa86220e9392c73f8036a283c9568a115e339d70
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-17 22:36:04 +0530

    Time: 9 ms (96.79%), Space: 66.9 MB (90.8%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.js
@@ -3,42 +3,25 @@
  * @return {number}
  */
 var largestRectangleArea = function(heights) {
-    function PSE(nums){
-        let ans = []
-        let stack = []
-        for(let i = 0; i < nums.length; i++){
-            while(stack.length && nums[stack[stack.length-1]] >= nums[i]){
-                stack.pop()
-            }
-
-            ans[i] = stack.length ? stack[stack.length-1] : -1
-            stack.push(i)
-        }
-        return ans
-    }
-
-    function NSE(nums){
-        let ans = []
-        let stack = []
-        for(let i = nums.length-1; i >= 0; i--){
-            while(stack.length && nums[stack[stack.length-1]] > nums[i]){
-                stack.pop()
-            }
-
-            ans[i] = stack.length ? stack[stack.length-1] : nums.length
-            stack.push(i)
-        }
-        return ans
-    }
-
-    let pse = PSE(heights)
-    let nse = NSE(heights)
-
+    let stack = []
     let max = 0
     for(let i = 0; i < heights.length; i++){
-        let ele = heights[i]
+        while(stack.length && heights[stack[stack.length-1]] >= heights[i]){
+            let ele = stack.pop()
+            let nse = i
+            let pse = stack.length ? stack[stack.length-1] : -1
 
-        max = Math.max(max, (ele * (nse[i] - pse[i] - 1)))
+            max = Math.max(max, heights[ele] * (nse - pse - 1))
+        }
+        stack.push(i)
+    }
+
+    while(stack.length){
+        let ele = stack.pop()
+        let pse = stack.length ? stack[stack.length-1] : -1
+        let nse = heights.length
+
+        max = Math.max(max, heights[ele] * (nse - pse - 1))
     }
 
     return max
