dff2b688ad48d80f56eeb83f8a73e9b902d5de5d
Jai-Sainiii <jaisaini6166@gmail.com>
2026-07-18 15:25:44 +0000

    Time: 49 ms (53.89%), Space: 72.7 MB (42.9%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.js
+++ 0901-online-stock-span/0901-online-stock-span.js
@@ -1,6 +1,6 @@
 
 var StockSpanner = function() {
-    this.stack = []
+    this.stack = [];
 };
 
 /** 
@@ -8,12 +8,12 @@ var StockSpanner = function() {
  * @return {number}
  */
 StockSpanner.prototype.next = function(price) {
-    let count = 1
+    let count = 1;
     while(this.stack.length && this.stack[this.stack.length - 1][0] <= price){
-        count += this.stack.pop()[1]
+        count += this.stack.pop()[1];
     }
-    this.stack.push([price, count])
-    return count
+    this.stack.push([price, count]);
+    return count;
 };
 
 /** 
