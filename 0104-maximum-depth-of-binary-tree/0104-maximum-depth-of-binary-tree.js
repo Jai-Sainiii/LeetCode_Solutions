b44e527c01684df56581a1d80b72d1bd37368446
Jai Saini <134952449+Jai-Sainiii@users.noreply.github.com>
2026-07-26 21:07:19 +0530

    Time: 3 ms (9.29%), Space: 63.6 MB (5.3%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(!root){
+        return 0;
+    }
+    let que = []
+    let depth = 0
+    que.push(root)
+    while(que.length){
+        let level = []
+        let size = que.length
+        for(let i = 0; i < size; i++){
+            let node = que.shift();
+            level.push(node.val);
+            if(node.left){
+                que.push(node.left)
+            }
+            if(node.right){
+                que.push(node.right)
+            }
+        }
+        depth++
+    }
+
+    return depth;
+};
