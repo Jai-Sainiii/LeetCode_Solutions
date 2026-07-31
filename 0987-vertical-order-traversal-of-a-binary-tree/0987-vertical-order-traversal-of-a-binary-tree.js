daf244dec7f2888c2e04cc97552c7b0bf8d090e8
Jai Saini <134952449+Jai-Sainiii@users.noreply.github.com>
2026-07-31 17:31:42 +0530

    Time: 0 ms (100%), Space: 56 MB (67.32%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.js
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.js
@@ -11,31 +11,31 @@
  * @return {number[][]}
  */
 var verticalTraversal = function(root) {
-    if(!root) return []
-    let que = [[root, 0, 0]]
-    let nodes = new Map()
+    if(!root) return [];
+    let que = [[root, 0, 0]];
+    let nodes = new Map();
     while(que.length){
-        let [temp, x, y] = que.shift()
+        let [temp, x, y] = que.shift();
 
-        if(!nodes.has(x)) nodes.set(x, new Map())
-        if(!nodes.get(x).has(y)) nodes.get(x).set(y, [])
-        nodes.get(x).get(y).push(temp.val)
+        if(!nodes.has(x)) nodes.set(x, new Map());
+        if(!nodes.get(x).has(y)) nodes.get(x).set(y, []);
+        nodes.get(x).get(y).push(temp.val);
 
-        if(temp.left) que.push([temp.left, x-1, y+1]) 
-        if(temp.right) que.push([temp.right, x+1, y+1]) 
+        if(temp.left) que.push([temp.left, x-1, y+1]);
+        if(temp.right) que.push([temp.right, x+1, y+1]);
     }
 
-    let ans = []
-    let sortedX = [...nodes.keys()].sort((a,b) => a-b)
+    let ans = [];
+    let sortedX = [...nodes.keys()].sort((a,b) => a-b);
 
     for(const x of sortedX){
-        let col = []
-        let sortedY = [...nodes.get(x).keys()].sort((a,b) => a-b)
+        let col = [];
+        let sortedY = [...nodes.get(x).keys()].sort((a,b) => a-b);
         for(const y of sortedY){
             col.push(...nodes.get(x).get(y).sort((a,b) => a-b));
         }
-        ans.push(col)
+        ans.push(col);
     }
 
-    return ans
+    return ans;
 };
