c3ca3f5336262145c192189c6bc60afd1f387df5
Jai Saini <134952449+Jai-Sainiii@users.noreply.github.com>
2026-07-31 16:16:47 +0530

    Time: 86 ms (68.18%), Space: 65.1 MB (36.36%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minimumPushes = function(word) {
+    let map = new Map()
+    for(const ch of word){
+        map.set(ch, (map.get(ch) || 0) + 1)
+    }
+
+    let sorted = [...map.entries()].sort((a, b) => b[1]-a[1]);
+    let sortedMap = new Map(sorted)
+
+    let ans = 0
+    let count = 0
+    for(const [key, value] of sortedMap){
+        ans += value * (Math.floor(count/8) + 1)
+        count++
+    }
+
+    return ans
+};
