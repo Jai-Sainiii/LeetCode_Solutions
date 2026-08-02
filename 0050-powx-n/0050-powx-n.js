3742adb8d34ca01a914541ca9606c10122c6a2cc
Jai Saini <134952449+Jai-Sainiii@users.noreply.github.com>
2026-08-02 19:41:30 +0530

    Time: 0 ms (100%), Space: 54.3 MB (22.08%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if(x === 0) return 0;
+    if(n === 1) return x;
+
+    function pow(x, n){
+        if(n <= 0) return 1.0;
+        if(n === 1) return x;
+
+        if(n%2 === 0){
+            return pow(x*x, n/2);
+        }else{
+            return x * pow(x, n-1);
+        }
+    }
+
+    if(n < 0){
+        return 1.0 / pow(x, -n)
+    }
+
+    return pow(x,n)
+};
