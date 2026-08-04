c60283e9763240e53b1b38704df4945e5abad161
Jai Saini <134952449+Jai-Sainiii@users.noreply.github.com>
2026-08-04 12:32:53 +0530

    Time: 1 ms (98.04%), Space: 56.2 MB (85.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,12 +3,20 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    function isAlpha(ch){
+        let char = ch.charCodeAt(0);
+        return (
+            (char >= 48 && char <= 57) || 
+            (char >= 97 && char <= 122)
+        )
+    }
+    
     let str = s.toLowerCase()
     let left = 0
     let right = str.length-1
     while(left < right){
-        while(left < right && !/[a-z0-9]/.test(str[left])) left++;
-        while(left < right && !/[a-z0-9]/.test(str[right])) right--;
+        while(left < right && !isAlpha(str[left])) left++;
+        while(left < right && !isAlpha(str[right])) right--;
         if(str[left] !== str[right]) return false;
         left++;
         right--;
