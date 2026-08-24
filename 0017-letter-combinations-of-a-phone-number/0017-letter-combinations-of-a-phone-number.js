9d8d823ba0b4ac0e325a50d584002f2d8ec2ed7d
Jai Saini <134952449+Jai-Sainiii@users.noreply.github.com>
2026-08-24 19:49:18 +0530

    Time: 0 ms (100%), Space: 53.7 MB (42.89%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -5,14 +5,14 @@
 var letterCombinations = function(digits) {
     let ans = [];
     const numpad = {
-        0:'', 1:'', 2:'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'
+        2:'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'
     }
     function combi(index, current){
         if(index === digits.length){
             ans.push(current);
             return;
         }
-        let keypadkeys = numpad[digits[index] - '0'];
+        let keypadkeys = numpad[digits[index]];
         for(let i = 0; i < keypadkeys.length; i++){
             combi(index+1, current + keypadkeys[i]);
         }
