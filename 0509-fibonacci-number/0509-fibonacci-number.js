0417668d3634697343ce8f797fc5b35f8aac9ce1
Jai Saini <134952449+Jai-Sainiii@users.noreply.github.com>
2026-08-29 21:04:14 +0530

    Time: 42 ms (81.06%), Space: 53.5 MB (65.42%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -6,5 +6,14 @@ var fib = function(n) {
     if(n === 0) return 0;
     if(n === 1) return 1;
 
-    return fib(n - 1) + fib(n - 2);
+    let dp = [];
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for(let i = 2; i <= n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
 };
