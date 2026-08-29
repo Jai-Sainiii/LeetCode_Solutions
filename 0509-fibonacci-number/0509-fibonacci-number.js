03289221b218fbc1b8b906cbf80203326d42e910
Jai Saini <134952449+Jai-Sainiii@users.noreply.github.com>
2026-08-29 21:06:13 +0530

    Time: 48 ms (63.98%), Space: 53.9 MB (30.2%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,17 +3,23 @@
  * @return {number}
  */
 var fib = function(n) {
+    //Egde cases
     if(n === 0) return 0;
     if(n === 1) return 1;
 
+    //Memorization in dp;
+    //dp array to store repeated results;
     let dp = [];
 
+    //Base cases to store
     dp[0] = 0;
     dp[1] = 1;
 
+    //iterate through to find ans and store via memorization;
     for(let i = 2; i <= n; i++){
         dp[i] = dp[i-1] + dp[i-2];
     }
 
+    // returning the found ans
     return dp[n];
 };
