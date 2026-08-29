20d6e2086e6d32a66f5be92f12ff5917a0eea724
Jai Saini <134952449+Jai-Sainiii@users.noreply.github.com>
2026-08-29 20:51:17 +0530

    Time: 10 ms (32.81%), Space: 57.5 MB (21.32%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -7,11 +7,6 @@ var wordBreak = function(s, wordDict) {
     let set = new Set(wordDict);
     let memo = new Map();
 
-    function check(start, end){
-        if(set.has(s.slice(start, end+1))) return true;
-        return false;
-    }
-
     function solve(index){
         if(index === s.length){
             return true;
@@ -20,7 +15,7 @@ var wordBreak = function(s, wordDict) {
         if(memo.has(index)) return memo.get(index);
 
         for(let i = index; i < s.length; i++){
-            if(check(index, i) && solve(i+1)){
+            if(set.has(s.slice(index, i+1)) && solve(i+1)){
                 memo.set(index, true);
                 return true;
             }
