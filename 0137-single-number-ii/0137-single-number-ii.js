65a3cfedfbf607f37a04daf7fa968bf9f1c06554
Jai Saini <134952449+Jai-Sainiii@users.noreply.github.com>
2026-09-12 22:39:25 +0530

    Time: 3 ms (72.16%), Space: 54.2 MB (96.49%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.js
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -3,11 +3,26 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    nums.sort((a,b) => a-b);
+    //Better solution #1
+    // nums.sort((a,b) => a-b);
 
-    for(let i = 1; i < nums.length; i = i + 3){
-        if(nums[i] !== nums[i-1]) return nums[i-1];
+    // for(let i = 1; i < nums.length; i = i + 3){
+    //     if(nums[i] !== nums[i-1]) return nums[i-1];
+    // }
+
+    // return nums[nums.length-1];
+
+
+    //Bit Manupulation 
+    let ans = 0;
+    for(let index = 0; index < 32; index++){
+        let count = 0;
+        for(let i = 0; i < nums.length; i++){
+            if(nums[i] & (1 << index)) count++;
+        }
+
+        if(count % 3 === 1) ans = ans | (1 << index);
     }
 
-    return nums[nums.length-1];
+    return ans;
 };
