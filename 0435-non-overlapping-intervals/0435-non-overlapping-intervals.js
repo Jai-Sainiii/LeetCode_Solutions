121446c2023532427a2b99687e770d4e04626dee
Jai Saini <134952449+Jai-Sainiii@users.noreply.github.com>
2026-09-22 22:23:16 +0530

    Time: 75 ms (67%), Space: 86.4 MB (53.02%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) {
+    if(intervals.length === 1) return 0;
+
+    intervals.sort((a,b) => a[1]-b[1]);
+
+    let cnt = 1, n = intervals.length;
+    let lastTime = intervals[0][1];
+    for(let i = 1; i < n; i++){
+        if(lastTime <= intervals[i][0]){
+            cnt++
+            lastTime = intervals[i][1];
+        }
+    } 
+    return n-cnt;
+};
